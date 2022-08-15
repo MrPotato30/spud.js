4c011a7b85213d495769a7d432a637750c9c1259
Zeptar <99463015+Zeptar1069@users.noreply.github.com>
2022-08-15 15:25:27 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+if(Number(+process.version.slice(1, 3)) < 16) throw new SpudJSError("NodeJS v16+ required");
+
+try {
+  require('discord.js');
+} catch (e) {
+	throw new SpudJSError("Discord.JS is required");
+}
+
+const { version: discordJSVersion } = require(require('path').join(
+	require.resolve('discord.js'),
+	'..',
+	'..',
+	'package.json'
+));
+
+if (Number(discordJSVersion.slice(0, 2)) < 13)
+	throw new SpudJSError('DiscordJS v13+ required');
+
 module.exports = {
   ButtonPagination: require("./functions/ButtonPagination"),
   ButtonPaginationBuilder: require("./functions/ButtonPaginationBuilder"),
